4e7b1efc88e1fad19e2b135fd1e96294476c04ab
natagoh <natagoh@stanford.edu>
2021-02-24 21:51:21 -0500

    starter files

---
+++ components/NavBar.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function NavBar() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
---
+++ screens/Home.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+import {SearchBar} from 'react-native-elements';
+import {Input} from 'react-native-elements';
+import {Button} from 'react-native-elements';
+
+const updateSearch = (search) => {};
+
+export default function Home() {
+  const search = 'test';
+  return (
+    <SafeAreaView style={{justifyContent: 'center', alignItems: 'center'}}>
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={updateSearch}
+        value={search}
+      />
+
+      <Button title="Solid Button" />
+
+      <Button title="Outline button" type="outline" />
+
+      <Button title="Clear button" type="clear" />
+      <Input placeholder="BASIC INPUT" />
+      <Text>Home!</Text>
+    </SafeAreaView>
+  );
+}
